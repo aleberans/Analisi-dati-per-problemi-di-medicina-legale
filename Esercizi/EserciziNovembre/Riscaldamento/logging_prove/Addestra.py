c32a409ecfa3433508ffa60d89965e61435d35fc
Alessandro Beranti <aleberans@icloud.com>
2019-11-20 22:29:05 +0100

    Uso funzioni con logging iniziale

--- Esercizi/EserciziNovembre/Riscaldamento/logging_prove/Addestra.py
+++ Esercizi/EserciziNovembre/Riscaldamento/logging_prove/Addestra.py
@@ -1,60 +1,83 @@
-from sklearn.svm import SVC
-import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score
-import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
-from scikitplot.metrics import plot_confusion_matrix
-from sklearn.preprocessing import StandardScaler, RobustScaler, QuantileTransformer
-from sklearn.decomposition import PCA
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-import pandas as pd
-
-
 def addestraSVC(X, y, c, gamma, kernel, dim):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=30, stratify=y)
 
+    # standardizzo i dati
     sc = StandardScaler()
+    X_train_standardizzato = sc.fit_transform(X_train)
+    X_test_standardizzato = sc.fit_transform(X_test)
 
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.fit_transform(X_test)
-
+    # applico la riduzione della dimensionalita'
     pca = PCA(n_components=dim)
 
-    X_train = pca.fit_transform(X_train)
-    X_test = pca.fit_transform(X_test)
+    X_train_ridotto = pca.fit_transform(X_train_standardizzato)
+    X_test_ridotto = pca.fit_transform(X_test_standardizzato)
+
+    # addestro la SVC
+    model = SVC(gamma=gamma, C=c, kernel=kernel)
+    model.fit(X_train_ridotto, y_train)
+
+    y_pred = model.predict(X_test_ridotto)
+
+    # calcolo lo score
+    scoreStandardizzato = accuracy_score(y_test, y_pred)
+
+    # Non standardizzo i  dati ma riduco subito la dimensionalita' e addestro la SVC
+    X_train_ridotto = pca.fit_transform(X_train)
+    X_test_ridotto = pca.fit_transform(X_test)
 
     model = SVC(gamma=gamma, C=c, kernel=kernel)
-    model.fit(X_train, y_train)
+    model.fit(X_train_ridotto, y_train)
 
-    y_pred = model.predict(X_test)
+    y_pred_not_standardizzato = model.predict(X_test_ridotto)
 
-    return accuracy_score(y_test, y_pred)
+    # calcolo lo score
+    score_not_standardizzato = accuracy_score(y_test, y_pred_not_standardizzato)
+
+    if scoreStandardizzato > score_not_standardizzato:
+        return (f'Lo score e stato calcolato standardizzando i dati, score: {scoreStandardizzato}')
+    else:
+        return (f'Lo score e stato calcolato non standardizzando i dati, score: {score_not_standardizzato}')
 
 
 def addrestraDecisionTreeClassifier(X, y, criterion, dim):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=30, stratify=y)
 
+    # standardizzo i dati
     sc = StandardScaler()
+    X_train_standardizzato = sc.fit_transform(X_train)
+    X_test_standardizzato = sc.fit_transform(X_test)
 
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.fit_transform(X_test)
-
+    # applico la riduzione della dimensionalita'
     pca = PCA(n_components=dim)
 
-    X_train = pca.fit_transform(X_train)
-    X_test = pca.fit_transform(X_test)
+    X_train_ridotto = pca.fit_transform(X_train_standardizzato)
+    X_test_ridotto = pca.fit_transform(X_test_standardizzato)
+
+    # addestro un DecisionTree
+    model = DecisionTreeClassifier(criterion=criterion)
+    model.fit(X_train_ridotto, y_train)
+
+    y_pred = model.predict(X_test_ridotto)
+
+    # calcolo lo score
+    scoreStandardizzato = accuracy_score(y_test, y_pred)
+
+    # Non standardizzo i  dati ma riduco subito la dimensionalita' e addestro un DecisionTree
+    X_train_ridotto = pca.fit_transform(X_train)
+    X_test_ridotto = pca.fit_transform(X_test)
 
     model = DecisionTreeClassifier(criterion=criterion)
-    model.fit(X_train, y_train)
+    model.fit(X_train_ridotto, y_train)
 
-    y_pred = model.predict(X_test)
+    y_pred_not_standardizzato = model.predict(X_test_ridotto)
 
-    return accuracy_score(y_test, y_pred)
+    # calcolo lo score
+    score_not_standardizzato = accuracy_score(y_test, y_pred_not_standardizzato)
+
+    if scoreStandardizzato > score_not_standardizzato:
+        return (f'Lo score e stato calcolato standardizzando i dati, score: {scoreStandardizzato}')
+    else:
+        return (f'Lo score e stato calcolato non standardizzando i dati, score: {score_not_standardizzato}')
 
 
 def addestraRandomForestClassifier(X, y, n_estimators, dim):
@@ -113,6 +136,28 @@ def addestraNaiveBayes(X, y, dim):
     X_test = pca.fit_transform(X_test)
 
     model = GaussianNB()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    return accuracy_score(y_test, y_pred)
+
+
+def addestraMLPClassifier(X, y, activation, learning_rate, dim, hidden_layer_sizes, max_iter):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=30, stratify=y)
+
+    sc = StandardScaler()
+
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.fit_transform(X_test)
+
+    pca = PCA(n_components=dim)
+
+    X_train = pca.fit_transform(X_train)
+    X_test = pca.fit_transform(X_test)
+
+    model = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, max_iter=max_iter, activation=activation,
+                          learning_rate=learning_rate)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -196,6 +241,23 @@ def trovaIperparametri(X, y, model, numero_dimensioni):
         params = {'reduce_dim__n_components': np.arange(1, numero_dimensioni, 1),
 
                   }
+    elif model == MLPClassifier:
+
+        steps = [
+            ('scaler', StandardScaler()),
+            ('reduce_dim', PCA()),
+            ('mlpClassifier', MLPClassifier(hidden_layer_sizes=(150, 100, 50), max_iter=300)),
+            # impostate cosi per provare a vedere se trova gli altri iperparametri
+        ]
+
+        pipeline = Pipeline(steps)
+
+        params = {'reduce_dim__n_components': np.arange(1, numero_dimensioni, 1),
+                  'mlpClassifier__activation': ['identity', 'logistic', 'tanh', 'relu'],
+                  'mlpClassifier__learning_rate': ['constant', 'invscaling', 'adaptive'],
+
+                  }
+
     grid = GridSearchCV(pipeline, param_grid=params, cv=3)
 
     grid.fit(X_train, y_train)
